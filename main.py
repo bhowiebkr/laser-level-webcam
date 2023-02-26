860fd56e178459d58bc7a493c82e332e19504c61
Bryan <bhowiebkr@gmail.com>
2023-02-26 17:02:12 -0500

    converting the graph to an image

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
 
+        self.resize(800, 600)
+
         # Create the left and right widgets
         self.left_widget = LeftWidget(self)
         self.right_widget = RightWidget(self)
@@ -34,29 +36,44 @@ class LeftWidget(QtWidgets.QWidget):
         if self.image is not None:
             qimage = QtGui.QImage(self.image.data, self.image.shape[1], self.image.shape[0], QtGui.QImage.Format_Grayscale8)
             pixmap = QtGui.QPixmap.fromImage(qimage)
-            scaled_pixmap = pixmap.scaled(self.width(), self.height(), QtCore.Qt.KeepAspectRatio)          
-            painter.drawPixmap(self.rect(), scaled_pixmap)
+            pixmap = pixmap.transformed(QtGui.QTransform().rotate(-90))
+            pixmap = pixmap.scaled(self.width(), self.height(), QtCore.Qt.KeepAspectRatio)          
+            painter.drawPixmap(self.rect(), pixmap)
 
     def setImage(self, image):
         self.image = image
         self.update()
 
-# Define the right widget to display the histogram of luminosity
+# Define the right widget to display the LuminosityScope of luminosity
 class RightWidget(QtWidgets.QWidget):
     def __init__(self, parent):
         super().__init__(parent)
-        self.histogram = None
+        self.LuminosityScope = None
 
     def paintEvent(self, event):
         painter = QtGui.QPainter(self)
-        if self.histogram is not None:
-            painter.setPen(QtCore.Qt.black)
-            painter.setBrush(QtGui.QColor(255, 0, 0, 127))
-            for i, count in enumerate(self.histogram):
-                painter.drawRect(i, self.height() - int(count), 1, int(count))
+        if self.LuminosityScope is not None:
+            #painter.setPen(QtCore.Qt.black)
+            #painter.setBrush(QtGui.QColor(255, 0, 0, 127))
+            #barThickness = int(float(self.height()) / self.LuminosityScope.shape[0])
+            #for i, count in enumerate(self.LuminosityScope):
+            #    painter.drawRect(0, i, int(count), barThickness)
 
-    def setHistogram(self, histogram):
-        self.histogram = histogram
+            # Defind the scope image day as the width (long side) of the image x 256 for pixels 
+            scopeData = np.zeros((self.LuminosityScope.shape[0], 256)).astype(np.uint8)
+
+            # Loop over intensity values and set scope data
+            for i, intensity in enumerate(self.LuminosityScope):
+                scopeData[i, :int(intensity)] = 255
+
+            qimage = QtGui.QImage(scopeData, scopeData.shape[0], scopeData.shape[1], QtGui.QImage.Format_Grayscale8)
+            pixmap = QtGui.QPixmap.fromImage(qimage)
+            scaled_pixmap = pixmap.scaled(self.width(), self.height(), QtCore.Qt.KeepAspectRatio)          
+            painter.drawPixmap(self.rect(), scaled_pixmap)
+
+
+    def setLuminosityScope(self, LuminosityScope):
+        self.LuminosityScope = LuminosityScope
         self.update()
 
 # Define the webcam thread to capture frames from the webcam and update the widgets
@@ -73,13 +90,13 @@ class WebcamThread(threading.Thread):
                 frame = webcam.get_next_data()
                 # Convert the RGB image to grayscale using the luminosity method
                 gray = np.dot(frame[...,:3], [0.2126, 0.7152, 0.0722]).astype(np.uint8)
+                intensity_values = np.mean(gray, axis=0)
 
-                intensity_values = np.mean(gray, axis=1)
-
+                print(intensity_values)
 
                 # Update the left and right widgets
                 self.parent.left_widget.setImage(gray)
-                self.parent.right_widget.setHistogram(intensity_values)
+                self.parent.right_widget.setLuminosityScope(intensity_values)
                 # Wait for a short time to avoid overloading the CPU
                 self.stop_event.wait(0.01)
 
