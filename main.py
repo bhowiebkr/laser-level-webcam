32b50c744464dfcf3b9b5cd30b44677d69e9c92d
Bryan <bhowiebkr@gmail.com>
2023-02-26 17:23:51 -0500

    corrected converting to pixmap, transorm the right way

--- main.py
+++ main.py
@@ -37,7 +37,6 @@ class LeftWidget(QtWidgets.QWidget):
             qimage = QtGui.QImage(self.image.data, self.image.shape[1], self.image.shape[0], QtGui.QImage.Format_Grayscale8)
             pixmap = QtGui.QPixmap.fromImage(qimage)
             pixmap = pixmap.transformed(QtGui.QTransform().rotate(-90))
-            pixmap = pixmap.scaled(self.width(), self.height(), QtCore.Qt.KeepAspectRatio)          
             painter.drawPixmap(self.rect(), pixmap)
 
     def setImage(self, image):
@@ -53,23 +52,22 @@ class RightWidget(QtWidgets.QWidget):
     def paintEvent(self, event):
         painter = QtGui.QPainter(self)
         if self.LuminosityScope is not None:
-            #painter.setPen(QtCore.Qt.black)
-            #painter.setBrush(QtGui.QColor(255, 0, 0, 127))
-            #barThickness = int(float(self.height()) / self.LuminosityScope.shape[0])
-            #for i, count in enumerate(self.LuminosityScope):
-            #    painter.drawRect(0, i, int(count), barThickness)
 
-            # Defind the scope image day as the width (long side) of the image x 256 for pixels 
+            # Defind the scope image data as the width (long side) of the image x 256 for pixels 
             scopeData = np.zeros((self.LuminosityScope.shape[0], 256)).astype(np.uint8)
 
             # Loop over intensity values and set scope data
             for i, intensity in enumerate(self.LuminosityScope):
                 scopeData[i, :int(intensity)] = 255
 
-            qimage = QtGui.QImage(scopeData, scopeData.shape[0], scopeData.shape[1], QtGui.QImage.Format_Grayscale8)
+            qimage = QtGui.QImage(scopeData, scopeData.shape[1], scopeData.shape[0], QtGui.QImage.Format_Grayscale8)
             pixmap = QtGui.QPixmap.fromImage(qimage)
-            scaled_pixmap = pixmap.scaled(self.width(), self.height(), QtCore.Qt.KeepAspectRatio)          
-            painter.drawPixmap(self.rect(), scaled_pixmap)
+            # Create a vertical flip transform
+            transform = QtGui.QTransform()
+            transform.scale(1, -1)
+            pixmap = pixmap.transformed(transform)
+
+            painter.drawPixmap(self.rect(), pixmap)
 
 
     def setLuminosityScope(self, LuminosityScope):
@@ -91,8 +89,6 @@ class WebcamThread(threading.Thread):
                 # Convert the RGB image to grayscale using the luminosity method
                 gray = np.dot(frame[...,:3], [0.2126, 0.7152, 0.0722]).astype(np.uint8)
                 intensity_values = np.mean(gray, axis=0)
-
-                print(intensity_values)
 
                 # Update the left and right widgets
                 self.parent.left_widget.setImage(gray)
