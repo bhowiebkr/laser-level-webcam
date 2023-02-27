dd5e992076dbdf8f3d788a5008e7c3042617982b
Bryan <bhowiebkr@gmail.com>
2023-02-27 17:19:35 -0500

    organizing in a group box and control widgets

--- laser_level_tool/GUI/sensor_feed.py
+++ laser_level_tool/GUI/sensor_feed.py
@@ -1,8 +1,20 @@
 import imageio
 import threading
+import subprocess
 import numpy as np
 
-from PyQt5.QtWidgets import QWidget, QSizePolicy
+from PyQt5.QtWidgets import (
+    QWidget,
+    QSizePolicy,
+    QGroupBox,
+    QHBoxLayout,
+    QVBoxLayout,
+    QFormLayout,
+    QSlider,
+    QPushButton,
+)
+from PyQt5.QtCore import Qt
+
 from PyQt5.QtGui import QPainter, QImage, QPixmap, QTransform
 
 SIZE = [640, 480]
@@ -10,10 +22,18 @@ SIZE = [640, 480]
 
 # Define the webcam thread to capture frames from the webcam and update the widgets
 class WebcamThread(threading.Thread):
-    def __init__(self, parent):
+    def __init__(self):
         super().__init__()
-        self.parent = parent
         self.stop_event = threading.Event()
+
+        self.analyser = None
+        self.sensor_feed = None
+
+    def setAnalyser(self, analyser):
+        self.analyser = analyser
+
+    def setSensorFeed(self, sensor_feed):
+        self.sensor_feed = sensor_feed
 
     def run(self):
         with imageio.get_reader("<video1>", size=(SIZE[0], SIZE[1])) as webcam:
@@ -51,8 +71,8 @@ class WebcamThread(threading.Thread):
                     pass
 
                 # Update the left and right widgets
-                self.parent.sensor_feed.setImage(gray)
-                self.parent.analyser.setLuminosityScope(intensity_values)
+                self.sensor_feed.widget.setImage(gray)
+                self.analyser.setLuminosityScope(intensity_values)
                 # Wait for a short time to avoid overloading the CPU
                 self.stop_event.wait(0.01)
 
@@ -62,8 +82,8 @@ class WebcamThread(threading.Thread):
 
 
 # Define the left widget to display the grayscale webcam feed
-class SensorFeed(QWidget):
-    def __init__(self, parent):
+class SensorFeedWidget(QWidget):
+    def __init__(self, parent=None):
         super().__init__(parent)
         self.image = None
         self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
@@ -84,3 +104,43 @@ class SensorFeed(QWidget):
     def setImage(self, image):
         self.image = image
         self.update()
+
+
+class SensorFeed(QGroupBox):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.setTitle("Sensor Feed")
+
+        # Layouts
+        main_layout = QVBoxLayout()
+        self.setLayout(main_layout)
+
+        params = QFormLayout()
+
+        # Widgets
+        self.widget = SensorFeedWidget()
+        self.brightness = QSlider(Qt.Horizontal)
+        self.contrast = QSlider(Qt.Horizontal)
+        self.gamma = QSlider(Qt.Horizontal)
+        extra_btn = QPushButton("Camera Device Controls")
+        extra_btn.setFixedHeight(40)
+
+        # add widgets
+        params.addRow("Brightness", self.brightness)
+        params.addRow("Contrast", self.contrast)
+        params.addRow("Gamma", self.gamma)
+
+        main_layout.addWidget(self.widget)
+        main_layout.addLayout(params)
+        main_layout.addWidget(extra_btn)
+
+        # Start the webcam thread
+        self.webcam_thread = WebcamThread()
+
+        # Logic
+        extra_btn.clicked.connect(self.extra_controls)
+
+    def extra_controls(self):
+        cmd = 'ffmpeg -f dshow -show_video_device_dialog true -i video="USB Camera"'
+        subprocess.Popen(cmd, shell=True)
