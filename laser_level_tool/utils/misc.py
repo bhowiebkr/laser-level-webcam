e5e589c344ea0b0fea8b1e9d8d6e1082d37153cb
Bryan <bhowiebkr@gmail.com>
2023-02-27 19:15:37 -0500

    function to change brightness contrast gamma

---
+++ laser_level_tool/utils/misc.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+
+def adjust_image(image, brightness=0, contrast=1, gamma=1):
+    """
+    Adjusts the brightness, contrast, and gamma of a grayscale image using NumPy.
+    """
+
+    # Compute the scaling factor for brightness
+    brightness_factor = 1.0 + (brightness * 100) / 255.0
+
+    # Compute the scaling factor for contrast
+    contrast_factor = (contrast - 1.0) / 255.0 * image.mean() + 1.0
+
+    # Compute the gamma factor
+    gamma_factor = 1.0 / gamma
+
+    # Apply brightness, contrast, and gamma correction in one step
+    adjusted_image = (
+        np.power(contrast_factor * image * brightness_factor / 255.0, gamma_factor)
+        * 255.0
+    )
+
+    # Clip the pixel values to the valid range of 0 to 255
+    adjusted_image = np.clip(adjusted_image, 0, 255).astype(np.uint8)
+
+    return adjusted_image
