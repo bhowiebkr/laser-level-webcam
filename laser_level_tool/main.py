a148104d7a3647346fb9bf70026e3ac80b64f537
Bryan <bhowiebkr@gmail.com>
2023-02-28 01:11:27 -0500

    adding a splitter and getting max res of the camera

--- laser_level_tool/main.py
+++ laser_level_tool/main.py
@@ -5,7 +5,7 @@ from PyQt5.QtWidgets import (
     QMainWindow,
     QWidget,
     QHBoxLayout,
-    QFormLayout,
+    QSplitter,
     QApplication,
 )
 from PyQt5.QtCore import Qt
@@ -13,12 +13,17 @@ from PyQt5.QtCore import Qt
 import qdarktheme
 from GUI.analyser import Analyser
 from GUI.sensor_feed import SensorFeed
+from GUI.sampler import Sampler
+
+from utils.misc import get_webcam_max_res
 
 
 # Define the main window
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+
+        self.max_res = get_webcam_max_res()
 
         self.resize(800, 600)
         self.setWindowTitle("Laser Level Webcam Tool")
@@ -28,9 +33,17 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(central_widget)
         self.layout = QHBoxLayout(central_widget)
 
-        # Create the left and right widgets
-        self.sensor_feed = SensorFeed(self)
-        self.analyser = Analyser(self)
+        splitter = QSplitter()
+        left_widget = QWidget()
+        left_layout = QHBoxLayout()
+        left_layout.setContentsMargins(0, 0, 0, 0)
+
+        left_widget.setLayout(left_layout)
+
+        self.sensor_feed = SensorFeed()
+        self.analyser = Analyser()
+        self.sampler = Sampler()
+        self.sampler.set_sensor_res(self.max_res)
 
         self.sensor_feed.webcam_thread.setAnalyser(self.analyser)
         self.sensor_feed.webcam_thread.setSensorFeed(self.sensor_feed)
@@ -50,8 +63,13 @@ class MainWindow(QMainWindow):
         self.sensor_feed.webcam_thread.start()
 
         # Add to layouts
-        self.layout.addWidget(self.sensor_feed)
-        self.layout.addWidget(self.analyser)
+
+        left_layout.addWidget(self.sensor_feed)
+        left_layout.addWidget(self.analyser)
+
+        splitter.addWidget(left_widget)
+        splitter.addWidget(self.sampler)
+        self.layout.addWidget(splitter)
 
     def closeEvent(self, event):
         self.sensor_feed.webcam_thread.stop()
