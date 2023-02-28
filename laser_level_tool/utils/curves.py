8064ca86407298faf7f6a21cd8803ebeb0254045
Bryan <bhowiebkr@gmail.com>
2023-02-27 19:16:09 -0500

    checking for divide by zero errors and turning off warnings due to fit_curve

--- laser_level_tool/utils/curves.py
+++ laser_level_tool/utils/curves.py
@@ -1,16 +1,34 @@
 from scipy.optimize import curve_fit
 import numpy as np
 
+import warnings
+
+warnings.filterwarnings("ignore")
+
+
 def fit_gaussian(curve):
     try:
+
         def gaussian(x, mean):
             amplitude = np.max(curve)
             stddev = np.std(curve)
-            return amplitude * np.exp(-((x - mean) / stddev) ** 2)
+
+            # Check if stddev is zero
+            if stddev == 0:
+                return 0
+
+            return amplitude * np.exp(-(((x - mean) / stddev) ** 2))
 
         x_data = np.arange(curve.size)
         y_data = np.asarray(curve)
-        popt, _ = curve_fit(gaussian, x_data, y_data, p0=(np.mean(x_data),), absolute_sigma=True, maxfev=1000)
+        popt, _ = curve_fit(
+            gaussian,
+            x_data,
+            y_data,
+            p0=(np.mean(x_data),),
+            absolute_sigma=True,
+            maxfev=1000,
+        )
         return popt[0]
     except:
         return None
