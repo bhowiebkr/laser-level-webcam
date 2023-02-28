1a4ea4591ee31fd99bde37240fe9ecb5d7caad78
Bryan <bhowiebkr@gmail.com>
2023-02-28 01:12:40 -0500

    a resolution widget

---
+++ laser_level_tool/GUI/widgets.py
@@ -0,0 +1,43 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QIntValidator
+from PyQt5.QtWidgets import QWidget, QHBoxLayout, QLabel, QLineEdit
+
+
+class ResolutionInputWidget(QWidget):
+    def __init__(self, parent=None, width=None, height=None):
+        super(ResolutionInputWidget, self).__init__(parent)
+
+        # Create the input fields
+        self._widthInput = QLineEdit()
+        self._widthInput.setValidator(QIntValidator())  # Only allow integer values
+        self._heightInput = QLineEdit()
+        self._heightInput.setValidator(QIntValidator())  # Only allow integer values
+
+        if width:
+            self._widthInput.setText(str(width))
+
+        if height:
+            self._heightInput.setText(str(height))
+
+        # Create the labels
+        widthLabel = QLabel("Width:")
+        heightLabel = QLabel("Height:")
+
+        # Create the layout
+        layout = QHBoxLayout()
+        layout.setContentsMargins(3, 0, 3, 0)
+        layout.addWidget(widthLabel)
+        layout.addWidget(self._widthInput)
+        layout.addWidget(heightLabel)
+        layout.addWidget(self._heightInput)
+        self.setLayout(layout)
+
+    def getWidth(self):
+        return int(self._widthInput.text()) if self._widthInput.text() else 0
+
+    def getHeight(self):
+        return int(self._heightInput.text()) if self._heightInput.text() else 0
+
+    def lock(self):
+        self._widthInput.setEnabled(False)
+        self._heightInput.setEnabled(False)
