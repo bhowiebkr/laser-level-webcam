993e87047f5cf1260403b268ba45bdd47f1532cf
Bryan <bhowiebkr@gmail.com>
2023-03-05 01:53:53 -0500

    scaler helper func

--- laser_level_tool/utils/misc.py
+++ laser_level_tool/utils/misc.py
@@ -2,6 +2,8 @@ import numpy as np
 
 from PySide6.QtMultimedia import QMediaDevices
 
+units_of_measurements = {"μm": 1000, "mil": 39.3701, "mm": 1}
+
 
 def adjust_image(image, brightness=0, contrast=1, gamma=1):
     """
@@ -18,10 +20,7 @@ def adjust_image(image, brightness=0, contrast=1, gamma=1):
     gamma_factor = 1.0 / gamma
 
     # Apply brightness, contrast, and gamma correction in one step
-    adjusted_image = (
-        np.power(contrast_factor * image * brightness_factor / 255.0, gamma_factor)
-        * 255.0
-    )
+    adjusted_image = np.power(contrast_factor * image * brightness_factor / 255.0, gamma_factor) * 255.0
 
     # Clip the pixel values to the valid range of 0 to 255
     adjusted_image = np.clip(adjusted_image, 0, 255).astype(np.uint8)
@@ -38,13 +37,14 @@ def get_webcam_max_res(index=0):
     supported_resolutions = camera_info.photoResolutions()
     max_resolution = None
     for resolution in supported_resolutions:
-        if max_resolution is None or (resolution.width() * resolution.height()) > (
-            max_resolution.width() * max_resolution.height()
-        ):
+        if max_resolution is None or (resolution.width() * resolution.height()) > (max_resolution.width() * max_resolution.height()):
             max_resolution = resolution
 
     return [max_resolution.width(), max_resolution.height()]
 
 
-if __name__ == "__main__":
-    print(get_webcam_max_res(1))
+def scale_center_point(sensor_width, data_width, center, zero, units):
+    val = float(sensor_width) / float(data_width) * (center - zero) * units_of_measurements[units]
+    print(data_width)
+    # print(f"sensor width: {sensor_width}\ndata width: {data_width}\ncenter: {center}\nzero: {zero}\nunits: {units}\nunit scale: {units_of_measurements[units]}\nval: {val}\n\n")
+    return val
