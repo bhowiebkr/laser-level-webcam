71b35707b625111536849313eda573522bea52ce
Bryan <bhowiebkr@gmail.com>
2023-03-06 01:19:41 -0500

    changed to a function as some units are funny
    Former-commit-id: 038bb6c71e4d3a0ad42678ce4c8fe780810cafdf

--- laser_level_tool/utils/misc.py
+++ laser_level_tool/utils/misc.py
@@ -2,7 +2,26 @@ import numpy as np
 
 from PySide6.QtMultimedia import QMediaDevices
 
-units_of_measurements = {"μm": 1000, "mil": 39.3701, "mm": 1}
+units_of_measurements = {"μm": 1000, "thou": 39.3701, "mil": 39.3701, "mm": 1, '"': 0.0393701, '0.0000"': 0.0393701, '0.00000"': 0.0393701}
+
+
+def get_units(unit, value):
+    if unit == '0.0000"':
+        return f'{"{:.4f}".format(value * 0.0393701)}"'
+    if unit == '0.00000"':
+        return f'{"{:.5f}".format(value * 0.0393701)}"'
+
+    if unit in units_of_measurements.keys():
+        return f'{"{:.2f}".format(value * units_of_measurements[unit])}{unit}'
+
+
+# print(get_units("mm", 3.5))
+# print(get_units("um", 3.5))
+# print(get_units("mil", 3.5))
+# print(get_units("thou", 3.5))
+# print(get_units('"', 3.5))
+# print(get_units('0.0000"', 3.5))
+# print(get_units('0.00000"', 3.5))
 
 
 def adjust_image(image, brightness=0, contrast=1, gamma=1):
@@ -45,5 +64,11 @@ def get_webcam_max_res(index=0):
 
 def scale_center_point(sensor_width, data_width, center, zero, units):
     val = float(sensor_width) / float(data_width) * (center - zero) * units_of_measurements[units]
+    # print(f"sensor width: {sensor_width}\ndata width: {data_width}\ncenter: {center}\nzero: {zero}\nunits: {units}\nunit scale: {units_of_measurements[units]}\nval: {val}\n\n")
+    return val
+
+
+def scale_center_point_no_units(sensor_width, data_width, center, zero):
+    val = float(sensor_width) / float(data_width) * (center - zero)
     # print(f"sensor width: {sensor_width}\ndata width: {data_width}\ncenter: {center}\nzero: {zero}\nunits: {units}\nunit scale: {units_of_measurements[units]}\nval: {val}\n\n")
     return val
