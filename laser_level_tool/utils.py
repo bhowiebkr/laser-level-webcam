85a33486c8e44aca74a6f2ac2044383cac96a3d4
Bryan <bhowiebkr@gmail.com>
2023-03-09 22:19:50 -0500

    A sample recalculation function that does calculations on the sample

--- laser_level_tool/utils.py
+++ laser_level_tool/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.stats import linregress
 
 from PySide6.QtMultimedia import QMediaDevices
 
@@ -13,15 +14,6 @@ def get_units(unit, value):
 
     if unit in units_of_measurements.keys():
         return f'{"{:.2f}".format(value * units_of_measurements[unit])}{unit}'
-
-
-# print(get_units("mm", 3.5))
-# print(get_units("um", 3.5))
-# print(get_units("mil", 3.5))
-# print(get_units("thou", 3.5))
-# print(get_units('"', 3.5))
-# print(get_units('0.0000"', 3.5))
-# print(get_units('0.00000"', 3.5))
 
 
 def adjust_image(image, brightness=0, contrast=1, gamma=1):
@@ -76,3 +68,28 @@ def scale_center_point_no_units(sensor_width, data_width, center, zero):
     val = float(sensor_width) / float(data_width) * (center - zero)
     # print(f"sensor width: {sensor_width}\ndata width: {data_width}\ncenter: {center}\nzero: {zero}\n")
     return val
+
+
+def samples_recalc(samples):
+    # check if there are at least three samples
+    if len(samples) >= 3:
+        # calculate the slope, intercept, and other parameters of a linear regression line
+        x = np.linspace(0, len(samples) - 1, len(samples))
+        y = np.array([s.y for s in samples])
+        slope, intercept, r_value, p_value, std_err = linregress(x, y)
+
+        # calculate the minimum and maximum errors
+        minYError, maxYError = np.inf, -np.inf
+        for s in samples:
+            s.linYError = np.abs(s.y - (slope * s.x + intercept))
+            if s.linYError > maxYError:
+                maxYError = s.linYError
+            if s.linYError < minYError:
+                minYError = s.linYError
+
+        # calculate the shim and scrape properties
+        for s in samples:
+            # make highest point zero for shimming, we are going to shim up all the low points to this height
+            s.shim = maxYError - s.linYError
+            # make lowest point zero for scraping, we are going to scrape off all the high areas
+            s.scrape = s.linYError - minYError
