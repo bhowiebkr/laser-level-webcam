ca09048cf15efcd7ee65cf328bd1f9a932dfc461
Bryan <bhowiebkr@gmail.com>
2023-09-10 23:13:37 -0400

    server testing with python pickle

--- src/s_server.py
+++ src/s_server.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 
+import pickle
 import socket
+
+HEADER_SIZE = 10
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 1234))
@@ -9,4 +12,9 @@ s.listen(5)
 while True:
     client_socket, address = s.accept()
     print(f"Connection from {address} has been established!")
-    client_socket.send(bytes("Welcome to the server!", "utf-8"))
+
+    d = {1: "Hey", 2: "There"}
+    msg = pickle.dumps(d)
+    msg = bytes(f"{len(msg):<{HEADER_SIZE}}", "utf-8") + msg
+
+    client_socket.send(msg)
