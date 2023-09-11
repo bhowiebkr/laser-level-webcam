9a0cf047eb4235222449c23d9cd9c1c6aa6cd81a
bhowiebkr <bhowiebkr@gmail.com>
2023-09-10 23:11:37 -0400

    updating socket client test with python pickle

--- src/s_client.py
+++ src/s_client.py
@@ -1,9 +1,32 @@
 from __future__ import annotations
 
+import pickle
 import socket
+
+HEADER_SIZE = 10
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("192.168.1.140", 1234))
 
-msg = s.recv(1024)
-print(msg.decode("utf-8"))
+while True:
+    full_msg = b""
+    new_msg = True
+
+    while True:
+        msg = s.recv(16)
+        if new_msg:
+            print(f"new message length: {msg[:HEADER_SIZE].decode('utf-8')}")
+            msglen = int(msg[:HEADER_SIZE])
+            new_msg = False
+
+        full_msg += msg
+
+        if len(full_msg) - HEADER_SIZE == msglen:
+            print("full msg recieved")
+            print(full_msg[HEADER_SIZE:])
+
+            d = pickle.loads(full_msg[HEADER_SIZE:])
+            print(d)
+
+            new_msg = True
+            full_msg = b""
